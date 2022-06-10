64eef7e75fdbda73470daf9204af72af8fac10f0
Vision3914 <106882984+Vision3914@users.noreply.github.com>
2022-06-10 11:48:49 -0400

    HorionInjector-SRC
    yep, its all open source, now take it.

---
+++ HorionInjector/ConsoleWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace HorionInjector
+{
+	// Token: 0x02000003 RID: 3
+	public partial class ConsoleWindow : Window
+	{
+		// Token: 0x06000004 RID: 4 RVA: 0x000020A4 File Offset: 0x000002A4
+		public ConsoleWindow()
+		{
+			this.InitializeComponent();
+		}
+
+		// Token: 0x06000005 RID: 5 RVA: 0x000020B2 File Offset: 0x000002B2
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			e.Cancel = true;
+			base.Hide();
+		}
+	}
+}
